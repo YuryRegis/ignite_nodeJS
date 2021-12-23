b47848f9482ce25f804b03189176e5b4dc69ba02
YuryRegis <yury_regis@hotmail.com>
2021-12-23 19:12:04 -0300

    ts-node config for debug

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,9 @@ app.get('/', (req, res) => {
     return(res.json({message: 'Hello World'}))
 })
 
+app.post('/courses', (req, res) => {
+    const {name} = req.body
+    return res.json({message: `Hello ${name}`})
+})
+
 app.listen(3333, () => console.log('Server started on port 3333!'))
